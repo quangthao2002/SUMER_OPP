76428ab81596b66d2ab83ba8754fb366b0af4985
quangthao <93909375+quangthao2002@users.noreply.github.com>
2023-06-23 11:23:59 +0700

    update

--- Tuan1/src/bai8/Demo.java
+++ Tuan1/src/bai8/Demo.java
@@ -8,13 +8,13 @@ public class Demo {
 		CD cd2 = new CD(2, "phim trinh tham", 5, 30000);
 		CD cd3 = new CD(3, "phim hanh dong", 16, 40000);
 		CD cd4 = new CD(4, "nhac thieu nhi", 15, 50000);
-		
+		CD cd5 = new CD(5, "nhac thieu nhi", 15, 50000);
 		CDList cdDlist = new CDList(4);
 		cdDlist.addCD(cd1);
 		cdDlist.addCD(cd2);
 		cdDlist.addCD(cd3);
 		cdDlist.addCD(cd4);
-		
+		cdDlist.addCD(cd5);
 		System.out.println("- Số lượng CD trong danh sách: " + cdDlist.getCount());
 		System.out.println("- Tổng giá thành của các CD: " + cdDlist.TotalCost());
 		System.out.println("---------------Thông tin các CD----------------------");
